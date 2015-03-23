70276ccc807d83412c43f234250ed4bf91d9988f
Daniel Stern <dstern@oicr.on.ca>
2015-03-23 09:54:10 -0400

    added more libs

--- js/summarize.js
+++ js/summarize.js
@@ -23,24 +23,3 @@ console.log(landMed);
 var landSD = d3.deviation(data, function(d) { return d.land_area; });
 console.log(landSD);
 
-d3.csv('data/expenses.csv',function(expenses){
-	var expensesByName = d3.nest()
-	  .key(function(d) { return d.name; })
-	  .entries(expenses);
-	
-	console.log(expensesByName);
-	
-	var expensesTotal = d3.nest()
-	  .key(function(d) { return d.name; })
-	  .rollup(function(v) { return d3.sum(v, function(d) { return d.amount; }); })
-	  .map(expenses);
-	console.log(JSON.stringify(expensesTotal));
-})
-
-var person = { name : "Birdman", occupation: "Imaginary Super Hero" };
-var templateString = "<div class='person'>" +
-  "  <span class='name'><%= name %></span>" +
-  "  <span class='occupation'><%= occupation %></span>" +
-  "</div>";
-var templateFunction = _.template(templateString);
-console.log(templateFunction(person))
